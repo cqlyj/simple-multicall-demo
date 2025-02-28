023efa53d861a3276500c4916c9c7a2298cc0790
cqlyj <luoyingjie0721@gmail.com>
2025-02-28 20:25:05 +0800

    feat: add unit tests for SimpleConfigurationManager's multicall functionality

---
+++ test/TestMulticall.t.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SimpleConfigurationManager} from "src/SimpleConfigurationManager.sol";
+
+contract TestMulticall is Test {
+    SimpleConfigurationManager public configManager;
+    address public owner = makeAddr("owner");
+
+    function setUp() external {
+        vm.prank(owner);
+        configManager = new SimpleConfigurationManager();
+    }
+
+    function testMulticallWorks() external {
+        bytes[] memory calls = new bytes[](3);
+        calls[0] = abi.encodeCall(configManager.setUintConfig, 1);
+        calls[1] = abi.encodeCall(
+            configManager.setAddressConfig,
+            address(this)
+        );
+        calls[2] = abi.encodeCall(
+            configManager.setAddressToUintConfig,
+            (address(this), 2)
+        );
+
+        vm.prank(owner);
+        configManager.multicall(calls);
+
+        assertEq(configManager.getUintConfig(), 1);
+        assertEq(configManager.getAddressConfig(), address(this));
+        assertEq(configManager.getAddressToUintConfig(address(this)), 2);
+    }
+}
