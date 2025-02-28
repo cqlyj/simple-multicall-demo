370fc304301c539fa08a778bc1f77eeaff221fc1
cqlyj <luoyingjie0721@gmail.com>
2025-02-28 20:45:44 +0800

    test: add test for reverting multicall with mixed payable and non-payable functions

--- test/TestMulticall.t.sol
+++ test/TestMulticall.t.sol
@@ -11,6 +11,7 @@ contract TestMulticall is Test {
     function setUp() external {
         vm.prank(owner);
         configManager = new SimpleConfigurationManager();
+        deal(owner, 1e18);
     }
 
     function testMulticallWorks() external {
@@ -31,5 +32,20 @@ contract TestMulticall is Test {
         assertEq(configManager.getUintConfig(), 1);
         assertEq(configManager.getAddressConfig(), address(this));
         assertEq(configManager.getAddressToUintConfig(address(this)), 2);
+    }
+
+    function testMulticallRevertWithPayableFunc() external {
+        bytes[] memory calls = new bytes[](3);
+        calls[0] = abi.encodeCall(configManager.setUintConfig, 1);
+        calls[1] = abi.encodeCall(
+            configManager.setAddressConfig,
+            address(this)
+        );
+        calls[2] = abi.encodeCall(configManager.addUint, 2);
+
+        // @notice you cannot mix payable and non-payable functions in a multicall
+        vm.expectRevert();
+        vm.prank(owner);
+        configManager.multicallPayable{value: 1e18}(calls);
     }
 }
